a8a043994ae0274dee46ea709509f33bf14da6a9
Marcin Polak <mapoart@gmail.com>
2021-03-24 10:06:34 +0100

    NEW FUNCTIONALITY: added nexss ahk ->  help, toExe, version. Now you can run ahk directly nexss ahk {your commads/params}

--- ahk.win32.nexss.config.js
+++ ahk.win32.nexss.config.js
@@ -13,10 +13,32 @@ languageConfig.compilers = {
     install: "scoop bucket add extras && scoop install autohotkey",
     command: "autohotkey.exe",
     args: "<file>",
-    help: ``
-  }
+    help: ``,
+  },
 };
 languageConfig.errors = require("./nexss.ahk.errors");
-languageConfig.languagePackageManagers = {};
+languageConfig.languagePackageManagers = {
+  ahk: {
+    messageAfterInstallation: "", //this message will be displayed after this package manager installation, maybe some action needed etc.
+    toExe: "Ahk2Exe.exe",
+    help: () => {
+      var url = "https://www.autohotkey.com/docs/AutoHotkey.htm";
+      var start =
+        process.platform == "darwin"
+          ? "open"
+          : process.platform == "win32"
+          ? "start"
+          : "xdg-open";
+      require("child_process").exec(start + " " + url);
+    },
+    // With anly AutoHotKey.exe doesn't work (why??) so we use nexss in nexss:
+    version: `nexss ${require("path").resolve("templates/version.ahk")}`,
+    // debug: "AutoHotkey.exe /Debug=localhost:9000",
+    init: () => {},
+    // if command not found in specification
+    // run directly on package manager
+    else: "AutoHotKey.exe",
+  },
+};
 
 module.exports = languageConfig;
